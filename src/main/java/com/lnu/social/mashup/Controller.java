f60a266031a26b7d602f8a02485c426274be9a97
rucsac <ruchirsachdeva@gmail.com>
2018-12-12 12:29:56 +0100

    code formatting

--- src/main/java/com/lnu/social/mashup/Controller.java
+++ src/main/java/com/lnu/social/mashup/Controller.java
@@ -26,16 +26,16 @@ public class Controller {
     @GetMapping("/search")
     public Collection<Analysis> getGenomicsAnalyses(@RequestParam("query") String query) {
         Collection<Analysis> analyses = new ArrayList<Analysis>();
-         try {
-        SearchResponse forObject = template.getForObject("https://www.googleapis.com/customsearch/v1?key=AIzaSyANNKdsj57oxTNR3LWExatX-l-t26OrhwI&cx=014793772949123529930:qdflpscf6y8&q="+query, SearchResponse.class);
-        for (Item item : forObject.getItems()) {
-            try {
-                analyses.add(webGenomicsClient.getGenomics(item.getLink()));
-            } catch (IOException e) {
+        try {
+            SearchResponse forObject = template.getForObject("https://www.googleapis.com/customsearch/v1?key=AIzaSyANNKdsj57oxTNR3LWExatX-l-t26OrhwI&cx=014793772949123529930:qdflpscf6y8&q=" + query, SearchResponse.class);
+            for (Item item : forObject.getItems()) {
+                try {
+                    analyses.add(webGenomicsClient.getGenomics(item.getLink()));
+                } catch (IOException e) {
+                }
             }
-        }
         } catch (Exception e) {
-            }
+        }
         return analyses;
     }
 }
